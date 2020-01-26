fe076079c2bcfb5613c6d0cd76b4c8735f732777
上工 <>
2020-01-26 17:46:42 +0800

    6. 色号设定

--- code/bootpack.c
+++ code/bootpack.c
@@ -1,15 +1,64 @@
 void io_hlt();
-void write_mem8(int addr, int data);
+void io_cli();
+void io_out8(int addr, int data);
+int io_load_eflags();
+void io_store_eflags(int data);
+
+
+void init_palette();
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(){
 	int i;
 
+	init_palette();
+
 	for(i = 0xa0000;i < 0xb0000;i++){
-		// write_mem8(i,15);
-		*((char *)i) = 15;
+		*((char *)i) = i&0xf;
 	}
 
 	for(;;){
 		io_hlt();
 	}
 }
+
+void init_palette(){
+	static unsigned char rgb[16*3] = {
+		0x00, 0x00, 0x00,	/*  0:黒 */
+		0xff, 0x00, 0x00,	/*  1:明るい赤 */
+		0x00, 0xff, 0x00,	/*  2:明るい緑 */
+		0xff, 0xff, 0x00,	/*  3:明るい黄色 */
+		0x00, 0x00, 0xff,	/*  4:明るい青 */
+		0xff, 0x00, 0xff,	/*  5:明るい紫 */
+		0x00, 0xff, 0xff,	/*  6:明るい水色 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:明るい灰色 */
+		0x84, 0x00, 0x00,	/*  9:暗い赤 */
+		0x00, 0x84, 0x00,	/* 10:暗い緑 */
+		0x84, 0x84, 0x00,	/* 11:暗い黄色 */
+		0x00, 0x00, 0x84,	/* 12:暗い青 */
+		0x84, 0x00, 0x84,	/* 13:暗い紫 */
+		0x00, 0x84, 0x84,	/* 14:暗い水色 */
+		0x84, 0x84, 0x84	/* 15:暗い灰色 */
+	};
+
+	set_palette(0,15,rgb);
+}
+
+void set_palette(int start, int end, char *rgb) {
+	int i;
+	int flag = io_load_eflags();
+	io_cli();
+	io_out8(0x3c8,start);
+	for(i = start;i<=end;i++){
+		io_out8(0x3c9,rgb[0]/4);
+		io_out8(0x3c9,rgb[1]/4);
+		io_out8(0x3c9,rgb[2]/4);
+		rgb+=3;
+	}
+	io_store_eflags(flag);
+}
+
+
+
+
--- code/naskfunc.nas
+++ code/naskfunc.nas
@@ -2,9 +2,77 @@
 [INSTRSET "i486p"]
 [BITS 32]
 [FILE "naskfunc.nas"]
-	GLOBAL _io_hlt,_write_mem8
+	GLOBAL _io_hlt,_io_cli,_io_sti,_io_stihlt
+	GLOBAL _io_in8,_io_in16,_io_in32
+	GLOBAL _io_out8,_io_out16,_io_out32
+	GLOBAL _io_load_eflags,_io_store_eflags
+
+	
 [SECTION .text]
 
 _io_hlt:
 	hlt
 	ret
+
+_io_cli:
+	cli
+	ret
+
+_io_sti:
+	sti
+	ret
+
+_io_stihlt:
+	sti
+	hlt
+	ret
+
+_io_in8:
+	mov edx,[esp+4]
+	mov eax,0
+	in al,dx
+	ret
+
+_io_in16:
+	mov edx,[esp+4]
+	mov eax,0
+	in ax,dx
+	ret
+
+_io_in32:
+	mov edx,[esp+4]
+	in eax,dx
+	ret
+
+_io_out8:
+	mov edx,[esp+4]
+	mov al,[esp+8]
+	out dx,al
+	ret
+
+_io_out16:
+	mov edx,[esp+4]
+	mov ax,[esp+8]
+	out dx,ax
+	ret
+
+_io_out32:
+	mov edx,[esp+4]
+	mov eax,[esp+8]
+	out dx,eax
+	ret
+
+_io_load_eflags:
+	pushfd
+	pop eax
+	ret
+
+_io_store_eflags:
+	mov eax,[esp+4]
+	push eax
+	popfd
+	ret
+
+
+
+
