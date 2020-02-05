7c09c813f0399b94e7026b435c624c644e330a8f
上工 <>
2020-02-05 17:19:14 +0800

    调整 main 代码

--- code/bootpack.c
+++ code/bootpack.c
@@ -1,9 +1,5 @@
 #include "bootpack.h"
 
-#define ADDR_SHTBACK 0xfec
-extern struct TIMECTL timerctl;
-
-struct FIFO32 fifo;
 
 void yiPrintf(char *chs){
     struct BOOTINFO *bInfo = (struct BOOTINFO *)ADR_BOOTINFO;
@@ -62,31 +58,7 @@ void task_b_main(struct SHEET *sht_back) {
 
 void HariMain(){
     
-    // 检查内存
-    struct MEMMAN *memman = (struct MEMMAN *)0x3c0000;  //#define MEMMAN_ADDR 0x3c0000
-    unsigned int memtotal = memtest(0x400000, 0xbfffffff);
-    memman_init(memman);
-    memman_free(memman, 0x1000, 0x9e000);
-    memman_free(memman, 0x400000, memtotal-0x400000);
-
-    struct TIMER *timer,*timer2;
-    
-    char s[100];
-    int fifobuf[128];
-	struct BOOTINFO *bInfo = (struct BOOTINFO *)ADR_BOOTINFO;
-	int xsize = bInfo->scrnx;
-	int ysize = bInfo->scrny;
-    
-    
-    struct MOUSE_DEC mdec;
-    
-    //初始化图册相关
-    struct SHTCTL *shtctl;
-    struct SHEET *sht_back,*sht_mouse,*sht_win;
-    unsigned char *buf_back,buf_mouse[256],*buf_win;
-    int cursor_x;
-    int data;
-    
+    //数据
     static char keytable[0x54] = {
         0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
         'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
@@ -95,79 +67,86 @@ void HariMain(){
         0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
         '2', '3', '0', '.'
     };
-    
-    
+    struct BOOTINFO *bInfo = (struct BOOTINFO *)ADR_BOOTINFO;
+    char s[100];
+    //缓冲区
+    struct FIFO32 fifo;
+    int fifobuf[128];
     fifo32_init(&fifo, 128, fifobuf,0);
     
-    shtctl = shtctl_init(memman, bInfo->vram, bInfo->scrnx, bInfo->scrny);
-    sht_back = sheet_alloc(shtctl);
-    sht_mouse = sheet_alloc(shtctl);
-    sht_win = sheet_alloc(shtctl);
-    buf_back = (unsigned char *)memman_alloc_4k(memman, bInfo->scrnx* bInfo->scrny);
-    buf_win = (unsigned char *)memman_alloc_4k(memman, 160*68);
-    sheet_setbuf(sht_back, buf_back, bInfo->scrnx, bInfo->scrny, -1);
-    sheet_setbuf(sht_win, buf_win, 160, 68, -1);
-    sheet_setbuf(sht_mouse, buf_mouse,  16, 16, 99);
+    // 检查内存
+    struct MEMMAN *memman = (struct MEMMAN *)0x3c0000;  //#define MEMMAN_ADDR 0x3c0000
+    unsigned int memtotal = memtest(0x400000, 0xbfffffff);
+    memman_init(memman);
+    memman_free(memman, 0x1000, 0x9e000);
+    memman_free(memman, 0x400000, memtotal-0x400000);
     
-    make_window8(buf_win, 160, 68, "window");
-    
-//    putfonts8_asc(buf_win, 160, 0, 30, COL8_RED, "welcom to");
-//    putfonts8_asc(buf_win, 160, 0, 50, COL8_RED, "YIOS");
-    make_textbox8(sht_win, 10, 30, sht_win->bxsize-20, 16, COL8_WHITE);
-    
-    sheet_updown(sht_back, 0);
-    sheet_updown(sht_win, 1);
-    sheet_updown(sht_mouse, 2);
-    
-
+    //初始化
     init_gdtidt();
     init_pic();
-    io_sti();   //这个地方产生了一个bug,调试了好久.....
-	init_palette();
-	init_screen8(buf_back, xsize, ysize);
+    init_palette();
     init_pit();
-
-    io_out8(PIC0_IMR, 0xf8);
-    io_out8(PIC1_IMR, 0xef);
-    
+    struct MOUSE_DEC mdec;
     init_keyboard(&fifo,256);
     enable_mouse(&fifo,512,&mdec);
-    
-    
-	//logo
-    boxfill8(buf_back, bInfo->scrnx, COL8_RED, 0, 0, 310, 18);
-	putfonts8_asc(buf_back, xsize,1,1,COL8_YELLOW,"HELLO YIOS");
-	putfonts8_asc(buf_back, xsize,0,0,COL8_YELLOW,"HELLO YIOS");
-    sheet_refresh(sht_back, 0, 0, 310, 18);
 
-
-	//鼠标
-    int mx = 160,my = 100;
-	init_mouse_cursor8(buf_mouse,99);
     
+    //初始化图册相关
+    struct SHTCTL *shtctl = shtctl_init(memman, bInfo->vram, bInfo->scrnx, bInfo->scrny);
     
-    // free 29304=632k(1m-4k(0开头的BIOS)-4k(后面的BIOS)-384k(0xa0000-0xaffff  显存用的地方 64k  后面的320我就不知道是干啥的了))+28m
-    sprintf(s, "[total %dM, free %dK]",memtotal/1024/1024,memman_total(memman)/1024);
-    putfonts8_asc_sht(sht_back, 0, 100, COL8_YELLOW, COL8_RED, s, 30);
-    
-
+    struct SHEET *sht_back = sheet_alloc(shtctl);
+    unsigned char *buf_back = buf_back = memman_alloc_4k(memman, bInfo->scrnx* bInfo->scrny);
+    sheet_setbuf(sht_back, buf_back, bInfo->scrnx, bInfo->scrny, -1);
+    init_screen8(buf_back, bInfo->scrnx, bInfo->scrny);
+    sheet_updown(sht_back, 0);
     sheet_slide(sht_back, 0, 0);
+    
+    struct SHEET *sht_mouse = sheet_alloc(shtctl);
+    unsigned char buf_mouse[256];
+    sheet_setbuf(sht_mouse, buf_mouse,  16, 16, 99);
+    sheet_updown(sht_mouse, 2);
+    int mx = 160,my = 100;
+    init_mouse_cursor8(buf_mouse,99);
     sheet_slide(sht_mouse, mx, my);
+    
+    struct SHEET *sht_win = sheet_alloc(shtctl);
+    unsigned char *buf_win = (unsigned char *)memman_alloc_4k(memman, 160*68);
+    sheet_setbuf(sht_win, buf_win, 160, 68, -1);
+    make_window8(buf_win, 160, 68, "window");
+    make_textbox8(sht_win, 10, 30, sht_win->bxsize-20, 16, COL8_WHITE);
+    sheet_updown(sht_win, 1);
     sheet_slide(sht_win, 80, 172);
     
-    timer = timer_alloc();
+    
+    //用于显示闪烁的光标
+    struct TIMER *timer = timer_alloc();
     timer_init(timer, &fifo, 0);
     timer_settime(timer,50);
     
-    timer2 =timer_alloc();
-    timer_init(timer2, &fifo, 2);
-    timer_settime(timer2,2);
+    
+    
+    //打开部分中断
+    io_sti();   //这个地方产生了一个bug,调试了好久.....
+    io_out8(PIC0_IMR, 0xf8);
+    io_out8(PIC1_IMR, 0xef);
+    
+    
+	//各种打印
+    boxfill8(buf_back, bInfo->scrnx, COL8_RED, 0, 0, 310, 18);
+	putfonts8_asc(buf_back, bInfo->scrnx,1,1,COL8_YELLOW,"HELLO YIOS");
+	putfonts8_asc(buf_back, bInfo->scrnx,0,0,COL8_YELLOW,"HELLO YIOS");
+    sheet_refresh(sht_back, 0, 0, 310, 18);
+    // free 29304=632k(1m-4k(0开头的BIOS)-4k(后面的BIOS)-384k(0xa0000-0xaffff  显存用的地方 64k  后面的320我就不知道是干啥的了))+28m
+    sprintf(s, "[total %dM, free %dK]",memtotal/1024/1024,memman_total(memman)/1024);
+    putfonts8_asc_sht(sht_back, 0, 100, COL8_YELLOW, COL8_RED, s, 30);
+
     
     //多任务
     struct TASK *task_a = task_init(memman);
     fifo.task = task_a;
     struct TASK *task_b = task_alloc();
     task_b->tss.esp = memman_alloc_4k(memman, 64*1024)+64*1024-8;
+    *((struct SHEET **)(task_b->tss.esp+4)) = sht_back;
     task_b->tss.eip = (int)task_b_main;
     task_b->tss.es = 1*8;
     task_b->tss.cs = 2*8;
@@ -175,11 +154,10 @@ void HariMain(){
     task_b->tss.ds = 1*8;
     task_b->tss.fs = 1*8;
     task_b->tss.gs = 1*8;
-    *((struct SHEET **)(task_b->tss.esp+4)) = sht_back;
     task_run(task_b);
     
-    
-    cursor_x = 8;
+    int cursor_x = 8;
+    int data;
 	for(;;){
         
         io_cli();
@@ -266,9 +244,6 @@ void HariMain(){
                 boxfill8(buf_win, sht_win->bxsize, COL8_WHITE, cursor_x, 30, cursor_x+2, 46);
                 timer_settime(timer, 50);
                 sheet_refresh( sht_win, cursor_x, 30, cursor_x+2, 46);
-            }else if(data==2){
-//                farjmp(0,4*8);
-//                timer_settime(timer2, 2);
             }
         }
 	}
