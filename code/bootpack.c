99addcdb61ada6b5d703edb6f31176d5a2b6d989
上工 <>
2020-02-07 16:01:03 +0800

    2. type 改良

--- code/bootpack.c
+++ code/bootpack.c
@@ -115,11 +115,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                             }
                         }
                         cursor_y = cons_newline(cursor_y, sheet);
-                    }else if (cmdline[0]=='t'&&
-                              cmdline[1]=='y'&&
-                              cmdline[2]=='p'&&
-                              cmdline[3]=='e'&&
-                              cmdline[4]==' ') {
+                    }else if (strncmp(cmdline, "type ", 5)==0) {
                         for (y = 0; y < 11; y++) {
                             s[y] = ' ';
                         }
@@ -163,11 +159,30 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                             for (x = 0; x < y; x++) {
                                 s[0] = p[x];
                                 s[1] = 0;
-                                putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, s, 1);
-                                cursor_x+=8;
-                                if (cursor_x==8+240) {
+                                if (s[0]==0x9) {
+                                    for (; ; ) {
+                                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, " ", 1);
+                                        cursor_x+=8;
+                                        if (cursor_x==8+240) {
+                                            cursor_x = 8;
+                                            cursor_y = cons_newline(cursor_y, sheet);
+                                        }
+//                                        if ((cursor_x-8)%32==0) {
+                                        if ((cursor_x-8)&0x1f==0) { //这两种都可以,下面这种应该要快些
+                                            break;
+                                        }
+                                    }
+                                }else if(s[0]==0xa){
                                     cursor_x = 8;
                                     cursor_y = cons_newline(cursor_y, sheet);
+                                }else if(s[0]==0xd){
+                                }else{
+                                    putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_WHITE, COL8_BLACK, s, 1);
+                                    cursor_x+=8;
+                                    if (cursor_x==8+240) {
+                                        cursor_x = 8;
+                                        cursor_y = cons_newline(cursor_y, sheet);
+                                    }
                                 }
                             }
                         }else{
