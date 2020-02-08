bf4646ad9a36a72cb8eaf41aba409a913e7c2695
上工 <>
2020-02-08 10:40:02 +0800

    2. 用c语言编写应用程序

---
+++ code/a.c
@@ -0,0 +1,13 @@
+//
+//  a.c
+//  YIOS_xcode
+//
+//  Created by 上工 on 2020/2/8.
+//  Copyright © 2020 YI. All rights reserved.
+//
+
+void api_putchar(int c);
+
+void HariMain(void){
+    api_putchar('Z');
+}
---
+++ code/hello3.c
@@ -0,0 +1,18 @@
+//
+//  hello3.c
+//  YIOS_xcode
+//
+//  Created by 上工 on 2020/2/8.
+//  Copyright © 2020 YI. All rights reserved.
+//
+
+
+void api_putchar(int c);
+
+void HariMain(void){
+    api_putchar('Z');
+    api_putchar('h');
+    api_putchar('a');
+    api_putchar('I');
+    api_putchar('1');
+}
