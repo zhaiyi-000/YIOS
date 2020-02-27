9b2103fc2280c349062446fcae4b62db09c83843
zhaiyi <>
2020-02-27 10:44:15 +0800

    6. 日本文字显示2

--- code/console.c
+++ code/console.c
@@ -59,6 +59,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
     }else{
         task->langmode = 0;
     }
+    task->langbyte1 = 0;
 
     for (;;) {
         io_cli();
@@ -128,6 +129,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
 void cons_newline(struct CONSOLE *cons){
     int x,y;
     struct SHEET *sheet = cons->sht;
+    struct TASK *task = task_now();
     if (cons->cur_y < 28+112) {
         cons->cur_y +=16;
     }else{
@@ -147,6 +149,9 @@ void cons_newline(struct CONSOLE *cons){
         }
     }
     cons->cur_x = 8;
+    if (task->langmode==1 && task->langbyte1 !=0) {
+        cons->cur_x+=8;
+    }
 }
 
 
@@ -294,6 +299,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline) {
             
             timer_cancelall(&task->fifo);
             memman_free_4k(memman, (int) q, segsiz);
+            task->langbyte1 = 0;
         }else{
             cons_putstr0(cons,".hrb file format error.\n");
         }
